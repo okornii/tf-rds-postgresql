131a84f5d1c330c3cd47f2e8bf772564cd3ea2e9
Oleksandr Korniienko <okornii@softserveinc.com>
2019-04-17 20:49:51 +0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,18 +2,8 @@ output "id" {
   value = "${aws_db_instance.postgresql.id}"
 }
 
-output "hosted_zone_id" {
-  value = "${aws_db_instance.postgresql.hosted_zone_id}"
-}
 
-output "hostname" {
-  value = "${aws_db_instance.postgresql.address}"
-}
 
 output "port" {
   value = "${aws_db_instance.postgresql.port}"
-}
-
-output "endpoint" {
-  value = "${aws_db_instance.postgresql.endpoint}"
 }
