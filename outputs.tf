e626a38b4f70553457023ff576e0d4ac600ba10b
Oleksandr Korniienko <okornii@softserveinc.com>
2019-04-17 16:15:49 +0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,10 +2,6 @@ output "id" {
   value = "${aws_db_instance.postgresql.id}"
 }
 
-output "database_security_group_id" {
-  value = "${aws_security_group.postgresql.id}"
-}
-
 output "hosted_zone_id" {
   value = "${aws_db_instance.postgresql.hosted_zone_id}"
 }
